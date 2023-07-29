b7331ee31daac42282fd1ca929797c275f02586e
Bill Duque <billduque34@gmail.com>
2023-07-29 15:46:41 +0800

    Added all the coding challenges that i have done

---
+++ week_2/11_container_most_water/index.js
@@ -0,0 +1,36 @@
+/* 
+    You are given an integer array height of length n. There are n vertical lines drawn such 
+    that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+    Find two lines that together with the x-axis form a container, 
+    such that the container contains the most water.
+
+    Return the maximum amount of water a container can store.
+
+    Notice that you may not slant the container.
+
+    Time complexity: O(n)
+*/
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0
+    let left = 0 
+    let right = height.length - 1
+
+    while (left < right) {
+        let result = Math.min(height[left], height[right]) * (right - left)
+        max = Math.max(max, result)
+        if (height[left] >= height[right]) {
+            right--
+        } else {
+            left++
+        }
+    }
+    return max
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxArea([1,1]));
