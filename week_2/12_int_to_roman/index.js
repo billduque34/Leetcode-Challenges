f23277dac2bdd2752eda0c85c1f76a7251096831
Bill Duque <billduque34@gmail.com>
2023-07-30 19:00:04 +0800

    Added 12 int to roman challenge

---
+++ week_2/12_int_to_roman/index.js
@@ -0,0 +1,21 @@
+/* 
+    The problem is in problem.txt
+    
+    Time complexity: O(1)
+*/
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX"]
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    const hrds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    const thds = ["", "M", "MM", "MMM"]
+
+    return thds[Math.floor(num/1000)%10] + hrds[Math.floor(num/100)%10] + tens[Math.floor(num/10)%10] + ones[Math.floor(num/1)%10]
+};
+
+console.log(intToRoman(3));
+console.log(intToRoman(58));
+console.log(intToRoman(1994));
